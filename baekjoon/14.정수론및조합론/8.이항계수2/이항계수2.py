116c1bacaf82827cae0ba942f05c81e72e9e0db7
jiminu <jmu417@gmail.com>
2022-08-31 11:22:14 +0900

    fix: 14-8

--- baekjoon/14.정수론및조합론/8.이항계수2/이항계수2.py
+++ baekjoon/14.정수론및조합론/8.이항계수2/이항계수2.py
@@ -2,13 +2,4 @@ import math
 
 N, K = map(int, input().split())
 
-cache = [[-1 for _ in range(N+1)] for _ in range(N+1)]
-
-def memoization(n, k) :
-    if cache[N][K] != -1 :
-        return cache[N][K]
-    
-    cache[N][K] = memoization(N+1, K) + memoization(N+1, K+1) 
-    return cache[N][K]
-
-print(memoization(N,K) % 10007)
+print((math.factorial(N) // math.factorial(K) // math.factorial(N-K)) % 10007)
