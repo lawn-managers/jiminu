9c7f89ef82ea19fb79acc7ea68e52e96e4b915d8
jiminu <jmu417@gmail.com>
2022-09-16 14:41:59 +0900

    add: 15-5 queen

--- baekjoon/15.백트래킹/5.N-Queen/N-Queen.py
+++ baekjoon/15.백트래킹/5.N-Queen/N-Queen.py
@@ -1,12 +1,13 @@
 N = int(input())
+
 pos = [-1 for _ in range(N)]
-prev_x = [-1 for _ in range(N)]
 result = 0
 x_start = 0
 
-if N == 1 : 
-    print('1')
-    exit()
+# if N == 1 : 
+#     print('1')
+#     exit()
+    
 def check_queen_position(x, y) :
     for pos_y, pos_x in enumerate(pos) :
         sub_y = abs(y - pos_y)
@@ -17,33 +18,29 @@ def check_queen_position(x, y) :
         if (pos_x == x or sub_x == sub_y) :
             return False
     return True
-
-def remove_prev(x, y) :
-    pos[y-1] = -1
     
 y = 0
-recrusion = 0
+recursion = 0
 number_type = ''
+state = True
 if N % 2 == 0 :
     number_type = 'even'
-    recrusion = N // 2
+    recursion = N / 2
 else :
     number_type = 'odd'
-    recrusion = (N // 2) + 1
+    recursion = (N / 2)
     
-while y < N :
-    if pos[0] >= recrusion :
-        break
+while state :
+    if pos[0] >= recursion :
+        state = False
+        result *= 2
+    
     for x in range(x_start, N) :
-        
         if check_queen_position(x, y) :
             pos[y] = x
-            prev_x[y] = x
             break
         
     if pos[y] == -1 :
-        if y == 0 and x_start == N:
-            break
         x_start = pos[y-1] + 1
         pos[y-1] = -1
         y -= 1
@@ -53,13 +50,20 @@ while y < N :
         pos[y] = -1
         pos[y-1] = -1
         y -= 1
-        if number_type == 'even' :
-            result += 2
-        else :
-            if pos[0] == recrusion-1 :
-                result += 1
-            else :
-                result += 2
+        result += 1
+        
+        # if pos[0] < recursion :
+        #     result += 2
+        # else : 
+        #     result += 1
+            
+        # if number_type == 'even' :
+        #     result += 2
+        # else :
+        #     if pos[0] == recursion-1 :
+        #         result += 1
+        #     else :
+        #         result += 2
             
     
     elif pos[y] != -1 :
