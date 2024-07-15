a29368e4f05e60c2c2a018242e702398c14b1dc2
jiminu <jmu417@gmail.com>
2024-07-15 19:32:26 +0900

    swap wrong number

--- sw/20936.상자정렬하기/20936.py
+++ sw/20936.상자정렬하기/20936.py
@@ -1,27 +1,41 @@
-def check_view(i, curr):
-    if (aparts_height[i-2] < curr and
-        aparts_height[i-1] < curr and
-        aparts_height[i+1] < curr and 
-        aparts_height[i+2] < curr):
+def equal_index_and_value(num_list, i):
+    if num_list[i-1] == i:
         return True
     return False
 
-
-for i in range(1, 11):
-    T = int(input())
-
-    aparts_height = list(map(int, input().split()))
-
-    answer = 0
-
-    for apart_queue in range(2, len(aparts_height)-2):
-        apart_height = aparts_height[apart_queue]
+def organize_list(num_list, curr_num):
+    num_list.append(len(num_list)+1)
+    next_index = len(num_list) - 1
+    moving_index = ''
+    count = 0
+    while(not(equal_index_and_value(num_list, curr_num))):
+        curr_index = num_list.index(curr_num) + 1
+        moving_index += f'{curr_index} '
+        num_list[curr_index-1], num_list[next_index] = num_list[next_index], num_list[curr_index-1]
         
-        for curr_floor in range(apart_height, 0, -1):
-            if check_view(apart_queue, curr_floor):
-                answer += 1
-            else:
-                break
-            
-    print(f'#{i} {answer}')
+        next_index = curr_index - 1
+        curr_num = curr_index
+        count += 1
+        
+    num_list.pop()
+    return num_list, count, moving_index
+    
 
+T = int(input())
+
+for i in range(T):
+    highest_number = int(input())
+    num_list = list(map(int, input().split()))
+    answer = ''
+    count = 0
+    
+    for curr_num in range(highest_number, 1, -1):
+        curr_index = num_list.index(curr_num) + 1
+        if equal_index_and_value(num_list, curr_num):
+            continue
+        
+        num_list, moving_count, moving_index = organize_list(num_list, curr_num)
+        answer += moving_index
+        count += moving_count
+    print(count)
+    print(answer)
