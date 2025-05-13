b812e3c037c085862fc07390469250b5688f1d6f
jiminu <jmu417@gmail.com>
2025-05-14 00:37:19 +0900

    ao

--- baekjoon/step/29.그래프와순회/1.깊이우선탐색1.py
+++ baekjoon/step/29.그래프와순회/1.깊이우선탐색1.py
@@ -2,10 +2,17 @@ import sys
 sys.setrecursionlimit(150000)
 N, M, R = map(int, sys.stdin.readline().split())
 
-graph = {i:[] for i in range(1, N+1)}
-visited = {i:0 for i in range(1, N+1)}
-count = 1
+# WARNING: [[]] * (N+1) will be copy themselves. then every array is the same.
+# NOTE: list
+# graph = [[]] * (N+1)
+graph = [[] for _ in range(N+1)]
+visited = [0] * (N+1)
 
+# NOTE: dictionary
+# graph = {i:[] for i in range(1, N+1)}
+# visited = {i:0 for i in range(1, N+1)}
+
+count = 1
 for _ in range(M):
     x, y = map(int, sys.stdin.readline().split())
     graph[x].append(y)
@@ -17,15 +24,13 @@ def dfs(R):
     for x in sorted(graph[R]):
         if not visited[x]:
             count += 1
-            graph[R].remove(x)
-            graph[x].remove(R)
             dfs(x)
             
-# for i in range(1, N + 1):
-#     graph[i].sort()
+dfs(R)
 
-if graph[1]:
-    dfs(R)
-
-for key, value in visited.items():
-    print(value)
+if isinstance(visited, list): 
+    for i in visited[1:]:
+        print(i)
+else:
+    for k, v in visited.items():
+        print(v)
