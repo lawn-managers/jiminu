94b90a311e7a884113fbface46431fc63b5a7706
jiminu <jmu417@gmail.com>
2025-05-14 00:13:51 +0900

    homework

--- baekjoon/step/29.그래프와순회/1.깊이우선탐색1.py
+++ baekjoon/step/29.그래프와순회/1.깊이우선탐색1.py
@@ -1,28 +1,31 @@
 import sys
-
+sys.setrecursionlimit(150000)
 N, M, R = map(int, sys.stdin.readline().split())
 
 graph = {i:[] for i in range(1, N+1)}
-visited = {i:False for i in range(1, N+1)}
+visited = {i:0 for i in range(1, N+1)}
+count = 1
 
 for _ in range(M):
     x, y = map(int, sys.stdin.readline().split())
     graph[x].append(y)
     graph[y].append(x)
 
-
-def dfs(V, E, R):
-    print(R)
-    visited[R] = True
+def dfs(R):
+    global count
+    visited[R] = count
     for x in sorted(graph[R]):
         if not visited[x]:
+            count += 1
             graph[R].remove(x)
             graph[x].remove(R)
-            dfs(V, E, x)
+            dfs(x)
             
+# for i in range(1, N + 1):
+#     graph[i].sort()
 
-dfs(graph, visited, R)
+if graph[1]:
+    dfs(R)
 
 for key, value in visited.items():
-    if not value:
-        print(0)
+    print(value)
