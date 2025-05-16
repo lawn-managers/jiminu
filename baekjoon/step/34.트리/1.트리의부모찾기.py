7fa9197a5ce5ec91d10b4c8ad8f43483722431da
jiminu <jmu417@gmail.com>
2025-05-16 20:26:15 +0900

    tree

--- baekjoon/step/34.트리/1.트리의부모찾기.py
+++ baekjoon/step/34.트리/1.트리의부모찾기.py
@@ -1,2 +1,12 @@
 import sys
 
+N = int(sys.stdin.readline())
+tree = [[] for _ in range(N+1)]
+
+for _ in range(N-1):
+    parent, child = map(int, sys.stdin.readline().split())
+    tree[parent].append(child)
+    tree[child].append(parent)
+    
+for i in tree[2:]:
+    print(i[0])
